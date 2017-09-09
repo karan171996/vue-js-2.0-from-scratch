19e7695152cf3b61c9e00b83920bf2fef78f5294
Karan171996 <krnsngh38@gmail.com>
2017-09-09 13:17:54 +0530

    Vue.js localhost work

---
+++ vueapp/src/components/Test.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="test">
+      <input type="text" v-model="title"></br>
+      <h1>{{title}}</h1>
+      <p v-if="showName">{{user.firstName}}</p>
+      <ul>
+          <li v-for="item in items">{{item.title}}</li>
+      </ul>
+      <button v-on:click="greet('Hello Karan')">Say Greetings</button>
+      </br>
+      <input type="text" v-on:keyup="pressKey">
+  </div>
+</template>
+
+<script>
+ export default {
+     name:'test',
+     data() {
+         return {
+             title:'Hello World',
+              user: {
+                firstName:'Karan',
+                lastName:'Singh'
+             },
+             showName:true,
+             items:[
+                 {title:'Item One'},
+                 {title:'Item Two'},
+                 {title:'Item Three'}
+             ]
+         }
+     },
+         methods:{
+             greet:function(greeting) {
+                 alert(greeting);
+             },
+         
+         pressKey:function(e){
+             console.log(e.target.value);
+         }
+         }
+ }
+</script>
+
+<style scoped>
+
+</style>
